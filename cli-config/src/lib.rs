af978d0ab453938dfe35d29144b65d18396f5572
Miraland Labs <miraland.labs@gmail.com>
2023-01-08 11:26:29 +0800

    add geyser-plugin-postgres

--- cli-config/src/lib.rs
+++ cli-config/src/lib.rs
@@ -46,7 +46,7 @@
 //!     .ok_or_else(|| anyhow!("unable to get config file path"))?;
 //! let mut cli_config = Config::load(&config_file)?;
 //! // Set the RPC URL to devnet
-//! cli_config.json_rpc_url = "https://api.devnet.solana.com".to_string();
+//! cli_config.json_rpc_url = "https://api.devnet-mln.miraland.top".to_string();
 //! cli_config.save(&config_file)?;
 //! # Ok::<(), anyhow::Error>(())
 //! ```
