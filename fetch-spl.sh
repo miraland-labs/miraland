dc31ec9da7342362a9b98f8c3d18e31719c47a7a
Miraland Labs <miraland.labs@gmail.com>
2023-05-07 11:19:20 +0800

    update fetch-spl.sh

--- fetch-spl.sh
+++ fetch-spl.sh
@@ -20,8 +20,8 @@ fetch_program() {
     return
   fi
 
-  if [[ -r ~/.cache/solana-spl/$so ]]; then
-    cp ~/.cache/solana-spl/"$so" "$so"
+  if [[ -r ~/.cache/solarti-spl/$so ]]; then
+    cp ~/.cache/solarti-spl/"$so" "$so"
   else
     echo "Downloading $name $version"
     so_name="spl_${name//-/_}.so"
@@ -29,21 +29,21 @@ fetch_program() {
       set -x
       curl -L --retry 5 --retry-delay 2 --retry-connrefused \
         -o "$so" \
-        "https://github.com/miraland-labs/miraland-program-library/releases/download/$name-v$version/$so_name"
+        "https://github.com/miraland-labs/solarti-program-library/releases/download/$name-v$version/$so_name"
     )
 
-    mkdir -p ~/.cache/solana-spl
-    cp "$so" ~/.cache/solana-spl/"$so"
+    mkdir -p ~/.cache/solarti-spl
+    cp "$so" ~/.cache/solarti-spl/"$so"
   fi
 
 }
 
-fetch_program token 3.5.0 Token4Q2B47VCdUy8u3rSTMMk2bGA1k7eN8qfKSzdiM BPFLoader2111111111111111111111111111111111
-fetch_program token-2022 0.6.0 Token8N5ecJeFxL83iFa2h7AgJ8AtufM7bbg63LrW89 BPFLoaderUpgradeab1e11111111111111111111111
-fetch_program memo  1.0.0 MemojWWmbFiRdUEQtRpMkeeyNB181Mr9uWEzdrgHUnc BPFLoader1111111111111111111111111111111111
-fetch_program memo  3.0.0 MemoE5FsL5zWDjihivRfHTpkR8RdviRbziKsfGS9Ntd BPFLoader2111111111111111111111111111111111
-fetch_program associated-token-account 1.1.2 ATAccPjxdgWfJKKN4PmfJ55FbEDEwD8zJUwVjuL9MuHy BPFLoader2111111111111111111111111111111111
-fetch_program feature-proposal 1.0.0 FeatQtFWK7aZCvBCVURnhLaUvrm2m8tPN4jHhriettbc BPFLoader2111111111111111111111111111111111
+fetch_program token 3.5.3 Token4Q2B47VCdUy8u3rSTMMk2bGA1k7eN8qfKSzdiM BPFLoader2111111111111111111111111111111111
+fetch_program token-2022 0.6.4 Token8N5ecJeFxL83iFa2h7AgJ8AtufM7bbg63LrW89 BPFLoaderUpgradeab1e11111111111111111111111
+fetch_program memo  1.0.1 MemojWWmbFiRdUEQtRpMkeeyNB181Mr9uWEzdrgHUnc BPFLoader1111111111111111111111111111111111
+fetch_program memo  3.0.4 MemoE5FsL5zWDjihivRfHTpkR8RdviRbziKsfGS9Ntd BPFLoader2111111111111111111111111111111111
+fetch_program associated-token-account 1.1.5 ATAccPjxdgWfJKKN4PmfJ55FbEDEwD8zJUwVjuL9MuHy BPFLoader2111111111111111111111111111111111
+fetch_program feature-proposal 1.0.1 FeatQtFWK7aZCvBCVURnhLaUvrm2m8tPN4jHhriettbc BPFLoader2111111111111111111111111111111111
 
 echo "${genesis_args[@]}" > spl-genesis-args.sh
 
