9c296b7d75abbb50df2656467a865dc13b15aad8
Miraland Labs <miraland.labs@gmail.com>
2024-11-05 14:48:45 +0800

    change deps version and part code for future upgrade

--- cli-output/src/display.rs
+++ cli-output/src/display.rs
@@ -1,7 +1,7 @@
 use {
     crate::cli_output::CliSignatureVerificationStatus,
     base64::{prelude::BASE64_STANDARD, Engine},
-    chrono::{Local, NaiveDateTime, SecondsFormat, TimeZone, Utc},
+    chrono::{DateTime, Local, SecondsFormat, TimeZone, Utc},
     console::style,
     indicatif::{ProgressBar, ProgressStyle},
     miraland_cli_config::SettingType,
@@ -715,10 +715,8 @@ pub fn new_spinner_progress_bar() -> ProgressBar {
 }
 
 pub fn unix_timestamp_to_string(unix_timestamp: UnixTimestamp) -> String {
-    match NaiveDateTime::from_timestamp_opt(unix_timestamp, 0) {
-        Some(ndt) => Utc
-            .from_utc_datetime(&ndt)
-            .to_rfc3339_opts(SecondsFormat::Secs, true),
+    match DateTime::from_timestamp(unix_timestamp, 0) {
+        Some(ndt) => ndt.to_rfc3339_opts(SecondsFormat::Secs, true),
         None => format!("UnixTimestamp {unix_timestamp}"),
     }
 }
